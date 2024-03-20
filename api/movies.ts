79e70f9f2b7e307a1da429b494077c3a0af21f70
hprasath2306 <hprasathoff935@gmail.com>
2024-03-20 06:11:15 +0530

    fetch movie details

--- api/movies.ts
+++ api/movies.ts
@@ -1,13 +1,16 @@
+const headers = {
+  accept: "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzJiYjBiYzk4ZjEyNDEwZmMyYjM2YzQ2NzQzNmI4NCIsInN1YiI6IjY1ZjhlODk2MzkxYjljMDE3YmM4MTQxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k_C6BnAGKy-OCeQWPJRMrbqaj7pYp-vBfiPvV8-z2SU",
+}
+
+
 export const TopRatedMovies = async () => {
   const url =
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
   const options = {
     method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzJiYjBiYzk4ZjEyNDEwZmMyYjM2YzQ2NzQzNmI4NCIsInN1YiI6IjY1ZjhlODk2MzkxYjljMDE3YmM4MTQxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k_C6BnAGKy-OCeQWPJRMrbqaj7pYp-vBfiPvV8-z2SU",
-    },
+    headers,
   };
   try {
     const response = await fetch(url, options);
@@ -16,4 +19,20 @@ export const TopRatedMovies = async () => {
   } catch (err) {
     console.error(err);
   }
+};
+
+export const fetchMovie = async (id: number) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers,
+  };
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+  }
+
 };
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,14 +1,25 @@
-import { useLocalSearchParams } from 'expo-router';
-import {View,Text} from 'react-native';
+import { fetchMovie } from "@/api/movies";
+import { useQuery } from "@tanstack/react-query";
+import { useLocalSearchParams } from "expo-router";
+import { View, Text, ActivityIndicator } from "react-native";
 
 const MovieDetails = () => {
-    const {id} = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
 
-    return(
-        <View>
-            <Text>{id}</Text>
-        </View>
-    )
-}
+  const {data,isLoading,error} = useQuery({
+    queryKey: ["movies", id],
+    queryFn: () => fetchMovie(id),
+  });
+
+  if(isLoading){
+    return <ActivityIndicator />
+  }
+
+  return (
+    <View>
+      <Text style={{fontSize:24, fontWeight:'500'}}>{data.title}</Text>
+    </View>
+  );
+};
 
 export default MovieDetails;
